9c86af9018b74aadc7776c9db87e945b22a8f793
Axis <erlia@naver.com>
2024-02-14 02:25:00 +0900

    Initial commit

---
+++ src/ScanfEx1.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class ScanfEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scanner=new Scanner(System.in);
+		
+//		int num=scanner.nextInt();  //숫자입
+//		
+//		System.out.println(num);
+//		command+shift+c  ->한번에 주석처리, sop+shift+space -단축키 설
+		
+//		String input=scanner.nextLine();  	//문자열입력
+//		int num=Integer.parseInt(input);	//정수로 변환
+		
+		int num=Integer.parseInt(scanner.nextLine()); //위의 2줄을 1줄로 표현
+		System.out.println(num);
+		
+		
+		
+	}
+
+}
