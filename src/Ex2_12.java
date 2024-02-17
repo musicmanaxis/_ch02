8d189c0a494236ac318eb0dba293156e227b6d8b
Axis <erlia@naver.com>
2024-02-18 01:09:13 +0900

    2

--- src/Ex2_12.java
+++ src/Ex2_12.java
@@ -3,7 +3,7 @@ public class Ex2_12 {
 	public static void main(String[] args) {
 		String str="3";
 		
-		System.out.println(+str.charAt(0)-'0');
+		System.out.println(str.charAt(0)-'0');
 		//문자열을 문자 3로 변환->str.charAt(0)  후 문자로 빼
 		System.out.println('3'-'0'+1);  //문자를 숫자로 바꾸고 난 다음 숫자더하기 
 		//문자3을 정수3으로 변환할려면 (-'0')을  해주면 된다.		
